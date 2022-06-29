652f92faa2e46f01026120c8762f1ab1ba70441d
AnnapooraniBabu <annapoorani.babumanikandan@fwsa.freshworks.com>
2022-06-29 11:38:35 +0530

    Uncommented lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'todos#index'
-  # post 'todos',:to=>'todos#create'
-  # get 'todos/:id', to: 'todos#destroy'
+  post 'todos',:to=>'todos#create'
+  get 'todos/:id', to: 'todos#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
